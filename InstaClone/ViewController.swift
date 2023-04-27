8f2cc9ad17cd9337b9570f28b86f2bd0ef3e5b78
Kushal <kushal29june@gmail.com>
2023-04-27 09:19:11 +0530

    Initial Commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Kushal Rana on 27/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
